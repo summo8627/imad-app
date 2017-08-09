69b47c72edaf7116126ccf5ae49206eda3066449
summo8627 <30747922+summo8627@users.noreply.github.com>
2017-08-09 21:18:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,8 +19,9 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/sumo-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
