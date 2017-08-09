2ae10acc2f777986d62212297ee63d2630bb4455
summo8627 <30747922+summo8627@users.noreply.github.com>
2017-08-09 21:38:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 var element=document.GetElementBy('main-text');
 element.innerHTML='hi!! it\'s me your sumanth \n this is my first app';
 
+
+var img=document.GetElementBy('sumo');
+img.onclick=function(){
+    img.style.margin='100px';
+};
