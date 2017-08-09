9f724af04be71736e21d128fd3045c109a98bf92
summo8627 <30747922+summo8627@users.noreply.github.com>
2017-08-09 21:52:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ element.innerHTML='hi!! it\'s me your sumanth \n this is my first web app...!!';
 var img=document.getElementById('sumo');
 var marginright=0;
 function moveleft(){
-    marginright=marginright+10;
+    marginright=marginright+5;
     img.style.margin=marginright+'px';
 }
 img.onclick=function(){
